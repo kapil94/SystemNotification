8c31a67bcf19c020566d07a1df597caa5e6a4860
kapil94 <kapilsharma647@gmail.com>
2021-04-15 11:34:15 +0530

    Adding a notification for every hour until 4 hrs from logged-in time

--- SystemNotification.py
+++ SystemNotification.py
@@ -1,6 +1,6 @@
 ########################system notification example####################################
 from plyer import notification
-import time
+import time,sys
 
 current_time=""
 target_time=""
@@ -11,7 +11,15 @@ local_time=time.ctime(seconds)
 current_time+=local_time[11:19]
 target_time+=str(int(current_time[0:2])+5)+current_time[2:len(current_time)]
 
+target_time_list=[]
 
+for i in range(1,5):
+	target_time_list.append(str(int(current_time[0:2])+i)+current_time[2:len(current_time)])
+
+count=len(target_time_list)
+
+def exit_loop():
+	sys.exit(0)
 
 while flag:
 	
@@ -20,16 +28,23 @@ while flag:
 	current_time=""
 	current_time+=local_time[11:19]
 		
-	
-	if current_time==target_time:
+	for i in range(0,len(target_time_list)):	
 		
-		notification.notify(
-    			title='Beep beep beep ....',
-    			message='Dude get a life!!',
-    			app_icon=None, 
-    			timeout=10,  
-			)	
+		if current_time==target_time_list[i]:
+		
+			notification.notify(
+	    			title='Beep beep beep ....',
+	    			message='Dude get a life!! '+str(i)+' hrs have passed since you logged in..',
+	    			app_icon=None, 
+	    			timeout=10,  
+				)	
+			count=count-1
+			
+			if count==0:
+				
+				exit_loop()
+				
 
 		flag=False
-		
+	
 	
